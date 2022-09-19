2c0f288d96017dbb5b16b5a1751bdd57207e1022
EyesackHoweird <112007977+EyesackHoweird@users.noreply.github.com>
2022-09-19 10:43:03 -0500

    Add files via upload

---
+++ IsaacAssignment1.js
@@ -0,0 +1,27 @@
+//Question 1 and 2
+let myNumber = 5;
+
+//Question 3
+let myOtherNumber = 5;
+
+//Question 4
+let myTotal = myNumber+myOtherNumber;
+
+//Question 5
+let myName = "";
+let myCombo = myTotal.toString();
+
+//Question 6
+let myOtherCombo = myTotal;
+
+//Question 7
+let myRemainder = myNumber%5;
+
+//Question 8
+let i = 2;
+
+//Question 9
+let int = 4;
+
+//Question 10
+let myBoolean = false;
